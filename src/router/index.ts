3f1d4addaaeaff242d2c8483ae352755ee033225
meixiang474 <1436098850@qq.com>
2021-04-18 01:26:40 +0800

    feat: 页面跳转取消路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,11 +5,25 @@ import {
   createWebHistory,
 } from "vue-router";
 import routes from "./routes";
+import { store } from "@/client/store";
+import { Canceler } from "axios";
+import * as Types from "@/store/constants";
 
 const isServer = typeof window === "undefined";
 
 const history = isServer ? createMemoryHistory() : createWebHistory();
 
 export default (): Router => {
-  return createRouter({ routes, history });
+  const router = createRouter({ routes, history });
+  router.beforeEach((to, from, next) => {
+    const { cancels } = store.state.cancel;
+    Object.keys(cancels).forEach((key) => {
+      if (cancels[key]) {
+        (cancels[key] as Canceler)();
+      }
+    });
+    store.commit(`cancel/${Types.CHNAGE_CANCELS}`, {});
+    next();
+  });
+  return router;
 };
